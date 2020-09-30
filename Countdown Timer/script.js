e39096201a754500c4dcfeb93fed052edd67cbe2
Deva <33352031+Vasudevatirupathinaidu@users.noreply.github.com>
2020-09-30 19:22:55 +0530

    Countdown Timer

---
+++ Countdown Timer/script.js
@@ -0,0 +1,37 @@
+
+const daysEl = document.querySelector('#days');
+const hoursEl = document.querySelector('#hours');
+const minutesEl = document.querySelector('#minutes');
+const secondsEl = document.querySelector('#seconds');
+
+
+const newYear = '1 Jan 2021';
+
+function countdown() {
+  const newYearsDate = new Date(newYear);
+  const currentDate = new Date();
+
+  const totalSeconds = Math.floor((newYearsDate - currentDate) / (1000));
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+
+  const minutes = Math.floor((totalSeconds / 60) % 60);
+
+  const seconds = Math.floor((totalSeconds) % 60);
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = formatTime(hours);
+  minutesEl.innerHTML = formatTime(minutes);
+  secondsEl.innerHTML = formatTime(seconds);
+  
+  function formatTime (time) {
+    return time < 10 ? `0${time}` : time;
+  }
+
+}
+
+countdown();
+
+setInterval(countdown, 1000);
